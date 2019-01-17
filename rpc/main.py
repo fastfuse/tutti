002285cc09d64024e2539445b061d83b4bdac2d8
fastfuse <banderstadt251@gmail.com>
2019-01-17 22:39:42 +0200

    backups first draft

--- rpc/main.py
+++ rpc/main.py
@@ -1,14 +1,11 @@
 import datetime
 import os
 
-from rpc.utils import upload_file, make_tarfile
+from utils import upload_file, make_tarfile
 
 if __name__ == '__main__':
 
-    print(os.path.abspath(__file__))
+    tar_name = f"backup-{datetime.datetime.now().replace(microsecond=0).isoformat()}"
+    tar_file = make_tarfile(tar_name, 'README.md', 'requirements.txt', extra='gz')
 
-    # tar_name = f"backup-{datetime.datetime.now().replace(microsecond=0).isoformat()}"
-
-    # tar_file = make_tarfile(tar_name, 'Dockerfile', 'README.md', 'app.py', 'requirements.txt', extra='gz')
-
-    # upload_file(tar_file, 'Backups')
+    upload_file(tar_file, 'Backups')
--- rpc/utils.py
+++ rpc/utils.py
@@ -1,43 +1,31 @@
 import tarfile
 
-import os.path
-import pickle
-
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
+class FolderSearchFailure(Exception):
+    pass
 
-def get_credentials():
+
+def get_credentialsntials():
     """
     Get oauth2 credentials
     """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
+    # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    store = file.Storage('token.json')
+    creds = store.get()
 
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server()
-
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
 
     return creds
-
 
 def upload_file(file_name, dest_folder=""):
     """
@@ -52,10 +40,14 @@ def upload_file(file_name, dest_folder=""):
     file_metadata = {"name": file_name}
 
     if dest_folder:
-        folders = service.files().list(q="mimeType='application/vnd.google-apps.folder'").execute()
-        destination_folder = [folder for folder in folders['files'] if folder['name'] == 'Backups'][0]
+        try:
+            folders = service.files().list(q="mimeType='application/vnd.google-apps.folder'").execute()
+            destination_folder = [folder for folder in folders['files'] if folder['name'] == dest_folder][0]
 
-        file_metadata.update(parents=[destination_folder['id']])
+            file_metadata.update(parents=[destination_folder['id']])
+
+        except IndexError:
+            raise FolderSearchFailure(f"Failed to get folder '{dest_folder}'")
 
     media = MediaFileUpload(file_name)
 
